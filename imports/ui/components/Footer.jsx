85cd8cee3b190edd35bc2ee10c15d91df9b70316
Servio Mora <servio.mora19@gmail.com>
2017-08-30 17:00:28 -0400

    done for the day

--- imports/ui/components/Footer.jsx
+++ imports/ui/components/Footer.jsx
@@ -2,15 +2,19 @@ import React from 'react';
 
 const Footer = () => (
   <div id="footer">
-    <div className="info">
-      <p className="address">
-        115 River Rd. Suite 151 <br />
-        Edgewater, NJ 07020
-      </p>
-      <p className="phone">(855)&nbsp;662-5879</p>
-      <div className="copyright">&copy;&nbsp;Ockupy&nbsp;2017.</div>
+    <div className="wrapper">
+      <div className="info">
+        <p className="address">
+          115 River Rd. Suite 151 <br />
+          Edgewater, NJ 07020
+        </p>
+        <p className="phone">(855)&nbsp;662-5879</p>
+        <div className="copyright">&copy;&nbsp;Ockupy&nbsp;2017.</div>
+      </div>
+      <div className="logo">
+        <h2>Ockupy</h2>
+      </div>
     </div>
-    <div className="logo"></div>
   </div>
 );
 
