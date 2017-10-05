f2cc477ab159ab8a488f63e519cc376578274ac0
Servio Mora <servio.mora19@gmail.com>
2017-10-05 10:24:35 -0400

    added video and adjusted slider buttons

--- imports/ui/components/Projects/ProjectVideo.jsx
+++ imports/ui/components/Projects/ProjectVideo.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const ProjectVideo = (props) => {
   return (
     <div className="project-video">
-      <iframe src={props.source} allowFullScreen></iframe>
+      <iframe src={props.source} frameBorder="0" allowFullScreen></iframe>
     </div>
   );
 }
